7aa353c8d5a6bf1b15c47df9b8521547073fc2d6
support1122 <support@flashfirejobs.com>
2025-11-18 02:06:49 +0530

    Update CalendlyModal.tsx

--- src/components/calendlyModal/CalendlyModal.tsx
+++ src/components/calendlyModal/CalendlyModal.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useState, useEffect } from "react";
 import { X, Calendar, CheckCircle } from "lucide-react";
 import { InlineWidget, useCalendlyEventListener } from "react-calendly";
